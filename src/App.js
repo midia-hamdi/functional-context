1eee7ff892599742d3e38bc06e227981563010de
midia-hamdi <midiahamdi367@gmail.com>
2023-12-20 09:54:47 +0330

    context in functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Main from './Main'
+import AuthContext from './auth-context'
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState({id : 9})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthContext.Provider value={user}>
+      <Main />
+    </AuthContext.Provider>
+  )
 }
 
-export default App;
